27fd93293d8d59b76004cb26550d83e632a52928
Oliver Agdal <53648573+agdal@users.noreply.github.com>
2024-04-07 18:54:50 +0200

    Comments

--- src/elementer/Map/Map.jsx
+++ src/elementer/Map/Map.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import useStyles from './styles';
 
 // Imports til mappet
-import GoogleMapReact from 'google-map-react';
+import GoogleMapReact from 'google-map-react'; // Vi henter google map api til react brug
 import { Paper, Typography, useMediaQuery } from '@material-ui/core';
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
 import Rating from '@material-ui/lab/Rating';
 
+// Vi laver vores variabler, som vi bruger til at sætte vores koordianter og boundaries på vores map
 const Map = ({setCoordinates, setBounds, coordinates, places}) => {
 
     const classes = useStyles();
@@ -17,18 +18,20 @@ const Map = ({setCoordinates, setBounds, coordinates, places}) => {
         <div className={classes.mapContainer}>
             <GoogleMapReact 
             bootstrapURLKeys={{ key: 'AIzaSyCRG0PR6gjMBGqSIo1YrzaRbr6iwYWSdfg' }}
-            defaultCenter={coordinates}
-            center={coordinates}
+            defaultCenter={coordinates} // Sætter mappet til default at starte på brugeren lokation
+            center={coordinates} // Center mappet til brugeren
             defaultZoom={14}
             margin={[50,50,50,50]}
 
+            // Hver gange siden ændre sig, blir onChange kaldt, og vi sætter vores nye koordinater og map boundaries.
             options={''}
             onChange={(e) => {
                 console.log(e);
-                setCoordinates({ lat: e.center.lat, lng: e.center.lng});
-                setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw});
+                setCoordinates({ lat: e.center.lat, lng: e.center.lng}); // Sætter nye koordinater
+                setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw}); // Sætter nye boundaries
             }}
             >
+                // .map gør at vi gør noget for en liste af værdier, her kører laver vi en liste over alle kendte lokationer/resturanter inde for mappet
                 {places?.map((place, i) => (
                     <div
                     className={classes.markerContainer}
